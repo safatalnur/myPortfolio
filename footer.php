cc9fd3d94921e7a79a543cec74861f3f3b50097a
Safat Alnur <safat@macbookpro.home>
2025-04-05 23:47:25 -0400

    Extensive modification on alnur theme

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
-        <footer>This is the footer</footer>
+        <footer>This is the footer
+            <?php 
+                wp_nav_menu( array (
+                    'theme_location' => 'secondary'
+                )); 
+            ?>
+        </footer>
         
         <?php wp_footer();?>
     </body>
